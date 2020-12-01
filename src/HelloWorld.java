cef2b2d52649ad3be339cd7fb8c29959cb4f2456
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-01 15:23:57 -0600

    hello world java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    static public void main(String[] args) {
+//        System.out.println("Hello World!");
+        System.out.print("Hello");
+        System.out.print(" World!");
+    }
+
+}

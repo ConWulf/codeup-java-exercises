6daf3daf6a3bccf9a4e18d00ab919df982ad33b6
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-02 10:00:30 -0600

    hello world and data type exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,61 @@ public class HelloWorld {
 
     static public void main(String[] args) {
 //        System.out.println("Hello World!");
-        System.out.print("Hello");
-        System.out.print(" World!");
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+//        String myString = "this is a string";
+//        String myString = "t";
+//        String myString = 3.14159;
+//        System.out.println(myString);
+//        long myNumber;
+//            myNumber = 123L;
+//        myNumber = 3.14; //Not right type need double
+//    float myDecimalNum = (float)  myNumber;
+//        System.out.println(myDecimalNum);
+//        myNumber = 123L; //correct type
+//        myNumber = 123; //assumes int, and num is smaller than the max for int
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);//increments after it prints x
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);//increments before it prints x
+//    System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o; //string cannot be cast to any number
+//        System.out.println(three);
+//        System.out.println(o);
+
+//        int x = 4;
+//        x += 4;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        int x = 2147483647;
+//        x += 1;
+//        System.out.println(x);
+//        System.out.println(Integer.MAX_VALUE);
+//
+//        byte y = 127;
+//        y+=5;
+//        System.out.println(y);
     }
 
 }

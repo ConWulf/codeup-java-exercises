cc3aa12a89bb6113704b36dcbfc5d836c641f6fd
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-03 13:49:06 -0600

    count backwards by ten using dowhile

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,11 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        long i = 0;
+        long i = 100;
        do {
            System.out.printf("\"i\" is: %d%n", i);
-           i+=2;
-       } while (i <= 100);
+           i-=5;
+       } while (i >= -10);
 
 
     }

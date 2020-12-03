1e764b6f3f6b7cab095a1c9c3e4f6595d2644899
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-02 18:15:36 -0600

    console io exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main (String[] args) {
+//        double PI = 3.14159;
+//        System.out.printf("The value of PI is %f", PI);
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+//        System.out.println("enter an integer:");
+//        int userInteger = scanner.nextInt();
+//        System.out.println(userInteger);
+
+//        System.out.println("enter 3 words:");
+//        String str1 = scanner.next();
+//        String str2 = scanner.next();
+//        String str3 = scanner.next();
+//        System.out.printf("%s %n%s %n%s", str1, str2, str3);
+
+
+//        System.out.println("enter a sentence");
+//        String userSentence = scanner.nextLine();
+//        System.out.println(userSentence);
+
+//System.out.println("enter a length and width:");
+//String width = scanner.nextLine();
+//String length = scanner.nextLine();
+//System.out.println("enter a height");
+//String height = scanner.nextLine();
+//float w = Float.parseFloat(width);
+//float l = Float.parseFloat(length);
+//float h = Float.parseFloat(height);
+//
+//System.out.printf("the area of the room is: %f %n", w*l);
+//System.out.printf("the perimeter of your room is %f%n", 2*l+2*w);
+//System.out.printf("the volume of your room is: %f %n", w*l*h);
+//System.out.printf("your width was %f, your length was %f and your height was %f", w, l, h);
+
+//        System.out.println("enter a length and width:");
+//        float width = scanner.nextFloat();
+//        float length = scanner.nextFloat();
+//        System.out.println("enter a height");
+//        float height = scanner.nextFloat();
+//
+//        System.out.printf("the area of the room is: %f %n", width*length);
+//        System.out.printf("the perimeter of your room is %f%n", 2*length+2*width);
+//        System.out.printf("the volume of your room is: %f %n", width*length*height);
+//        System.out.printf("your width was %f, your length was %f and your height was %f", width, length, height);
+
+
+//        float e = .1F;
+//        float f = .2F;
+//        double a = .1;
+//        double b = .2;
+//        System.out.println(Float.sum(e, f)  == 0.3);
+//        System.out.println(Float.sum(e, f));
+//        System.out.println(e + f);
+//        System.out.println(a + b);
+//        System.out.println(.8 + .9);
+    }
+
+}

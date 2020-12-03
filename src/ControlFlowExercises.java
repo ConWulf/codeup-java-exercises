2e2fd9154ec8672dccba27601c40617706f11f9a
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-03 15:27:48 -0600

    number to letter grade

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -16,7 +16,28 @@ public class ControlFlowExercises {
             System.out.printf("%n%d     |%d     |%d", i, (int)squared, (int)cubed);
         }
         System.out.println("\nwould you lake to continue[Y/N]?");
-        } while ((scanner.hasNextLine() && scanner.next().equalsIgnoreCase("y")) || (!scanner.hasNextLine() && scanner.next().equalsIgnoreCase("yes")));
+        } while (scanner.next().equalsIgnoreCase("y") || (!scanner.hasNextLine() && scanner.next().equalsIgnoreCase("yes")));
+
+//        System.out.println("What is your number grade?");
+//        int userGrade = scanner.nextInt();
+//        System.out.println("your letter grade is: ");
+//        if (userGrade <= 100 && userGrade >= 95) {
+//            System.out.print("A+");
+//        } else if (userGrade <= 94 && userGrade >= 88) {
+//            System.out.print("A-");
+//        } else if (userGrade <= 87 && userGrade >= 84) {
+//            System.out.print("B+");
+//        } else if (userGrade <= 83 && userGrade >= 80) {
+//            System.out.print("B-");
+//        } else if (userGrade <= 79 && userGrade >= 73) {
+//            System.out.print("C+");
+//        } else if (userGrade <= 73 && userGrade >= 67) {
+//            System.out.print("C-");
+//        } else if (userGrade <= 66 && userGrade >= 60) {
+//            System.out.print("D");
+//        } else if (userGrade <= 59 && userGrade >= 0) {
+//            System.out.print("F");
+//        }
 
 
     }

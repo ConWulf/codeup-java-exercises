37bb343103350accc09d707a09bf6a6bd7d51478
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-03 14:03:07 -0600

    refactored the previous two exercises to use for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,12 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        double i = 2;
-       do {
-           System.out.printf("\"i\" is: %f%n", i);
-           i = Math.pow(i, 2); //returns double
-       } while (i <= 1000000);
-
+        for (int i = 100; i >= -10; i-=5) {
+            System.out.printf("i is: %d%n", i);
+        }
+        for (double i = 2; i <= 1000000; i = Math.pow(i, 2)) {
+            System.out.printf("i is: %f%n", i);
+        }
 
     }
 

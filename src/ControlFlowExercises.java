1e219440fcd937e9e7d73a3496a8beb86ae1d815
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-03 14:19:12 -0600

    fizzbuzz challenge

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,27 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        for (int i = 100; i >= -10; i-=5) {
-            System.out.printf("i is: %d%n", i);
-        }
-        for (double i = 2; i <= 1000000; i = Math.pow(i, 2)) {
-            System.out.printf("i is: %f%n", i);
+        int i = 1;
+       do {
+           if (i % 3 == 0 && i % 5 == 0) {
+               System.out.printf("FizzBuzz %d%n", i);
+           } else if (i % 3 == 0) {
+               System.out.printf("Fizz %d%n", i);
+           } else if (i % 5 == 0) {
+               System.out.printf("Buzz %d%n", i);
+           }
+           i +=1; //returns double
+       } while (i <= 100);
+
+        for (int j = 1; j <= 100; j++) {
+            if (j % 3 == 0 && j % 5 == 0) {
+                System.out.printf("FizzBuzz %d%n", j);
+            } else if (j % 3 == 0) {
+                System.out.printf("Fizz %d%n", j);
+            } else if (j % 5 == 0) {
+                System.out.printf("Buzz %d%n", j);
+            }
+
         }
 
     }

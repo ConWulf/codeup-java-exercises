2d359a02fe4fe2b2d437d58d779eb5e31fd419db
Connor Wulf <connor.m.wulf@gmail.com>
2020-12-10 15:46:53 -0600

    arrays lec

---
+++ src/ArraysExercises.java
@@ -0,0 +1,19 @@
+import java.time.Period;
+
+public class ArraysExercises {
+
+
+    public static void main(String[] args) {
+//    int[] numbers = {1, 2, 3, 4, 5};
+//System.out.println(numbers);
+
+
+        String[] personArr = new String[3];
+
+
+
+    }
+
+
+
+}
